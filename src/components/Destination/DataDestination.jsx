7a13e3cd4449763498f93d9336222e45fc596c54
Walther-git <dev.w.work@gmail.com>
2023-08-08 11:25:02 -0500

    functional destination no optimized

---
+++ src/components/Destination/DataDestination.jsx
@@ -0,0 +1,6 @@
+  import { destinations } from '../../../public/assets/data.json'
+
+  const [moon, mars, europa, titan] = destinations
+  export const initialTabs = [moon, mars, europa, titan]
+  
+  
--- src/components/Destination/DestinationPicks.jsx
+++ src/components/Destination/DestinationPicks.jsx
@@ -1,22 +1,32 @@
-import "./styles.css";
+import "./DestinationPicks.css";
 import { useState } from "react";
-import { initialTabs as tabs } from "./ingredients";
+import { initialTabs as tabs } from "./DataDestination.jsx";
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function App() {
   const [selectedTab, setSelectedTab] = useState(tabs[0]);
 
   return (
-    <div className="window">
+    <motion.div
+      className="window"
+      initial={{ opacity: 0, y: '-30px' }}
+      transition={{ duration: 0.4 }}
+      animate={{ opacity: 1, y: '0px' }}
+    >
+      <div className='flex-row'>
+        <span className='number font-barlow font-style line-height'>01</span>
+        <p className='first-title font-barlow font-style line-height'>PICK YOUR DESTINATION</p>
+      </div>
+
       <nav className="navigation">
-        <ul className="ul-class">
+        <ul className="ul-class font-barlow font-style line-height">
           {tabs.map((item) => (
             <li
-              key={item.label}
-              className={`li-class ${item === selectedTab ? "selected" : ""}`}
+              key={item.name}
+              className={`li-class ${item === selectedTab ? "selected" : ""} font-barlow font-style line-height`}
               onClick={() => setSelectedTab(item)}
             >
-              {`${item.icon} ${item.label}`}
+              {`${item.name}`}
               {item === selectedTab ? (
                 <motion.div className="underline" layoutId="underline" />
               ) : null}
@@ -24,19 +34,48 @@ export default function App() {
           ))}
         </ul>
       </nav>
+
       <main className="destionation-main">
         <AnimatePresence mode='wait'>
           <motion.div
-            key={selectedTab ? selectedTab.label : "empty"}
+            className='flex-colum'
+            key={selectedTab ? selectedTab.name : "empty"}
             initial={{ y: 10, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -20, opacity: 0 }}
+            exit={{ y: -15, opacity: 0 }}
             transition={{ duration: 0.2 }}
           >
-            {selectedTab ? selectedTab.icon : "😋"}
+            <img className='planet' src={selectedTab.images.webp} />
+
+            <div className="box-text">
+              <h1 className='title-planet'>
+                {selectedTab.name}
+              </h1>
+
+              <p className='description'>
+                {selectedTab.description}
+              </p>
+
+              <div className='box-avg-est'>
+                <div className='box-avg'>
+                  <p className='distance font-barlow font-style line-height'>AVG. DISTANCE</p>
+                  <p className='distance-number font-style line-height'>
+                    {selectedTab.distance}
+                  </p>
+                </div>
+
+                <div className='box-est'>
+                  <p className='distance font-barlow font-style line-height'>EST. TRAVEL TIME</p>
+                  <p className='distance-number font-style line-height'>
+                    {selectedTab.travel}
+                  </p>
+                </div>
+              </div>
+            </div>
+
           </motion.div>
         </AnimatePresence>
       </main>
-    </div>
+    </motion.div>
   );
 }
